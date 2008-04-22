2b8f796363b9875beda019b06e75a0e3ac3419f4
neadam <sha1-94c70307ccce623c4c75bc33ad082dccd1c6c3c6@cern.ch>
2008-04-22 17:00:31 +0000

    Added Histogrammer to SealModule.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -4,7 +4,9 @@
 #include "MuonAnalysis/TagAndProbe/interface/TagProbeProducer.h"
 #include "MuonAnalysis/TagAndProbe/interface/MatchedProbeMaker.h"
 #include "MuonAnalysis/TagAndProbe/interface/TagProbeAnalyzer.h"
+#include "MuonAnalysis/TagAndProbe/interface/Histogrammer.h"
 
 DEFINE_ANOTHER_FWK_MODULE(TagProbeProducer);
 DEFINE_ANOTHER_FWK_MODULE(MatchedProbeMaker);
 DEFINE_ANOTHER_FWK_MODULE(TagProbeAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(Histogrammer);
