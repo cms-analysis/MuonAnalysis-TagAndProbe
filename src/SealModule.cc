88aac8b2381097f113c24cc0bb600dac552f68af
neadam <sha1-94c70307ccce623c4c75bc33ad082dccd1c6c3c6@cern.ch>
2008-05-11 12:13:19 +0000

    Added new TagProbe EDM compatible producer & ananlyzer.

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -2,11 +2,15 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "MuonAnalysis/TagAndProbe/interface/TagProbeProducer.h"
+#include "MuonAnalysis/TagAndProbe/interface/TagProbeEDMNtuple.h"
 #include "MuonAnalysis/TagAndProbe/interface/MatchedProbeMaker.h"
 #include "MuonAnalysis/TagAndProbe/interface/TagProbeAnalyzer.h"
+#include "MuonAnalysis/TagAndProbe/interface/TagProbeAnalysis.h"
 #include "MuonAnalysis/TagAndProbe/interface/Histogrammer.h"
 
 DEFINE_ANOTHER_FWK_MODULE(TagProbeProducer);
 DEFINE_ANOTHER_FWK_MODULE(MatchedProbeMaker);
 DEFINE_ANOTHER_FWK_MODULE(TagProbeAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(TagProbeAnalysis);
+DEFINE_ANOTHER_FWK_MODULE(TagProbeEDMNtuple);
 DEFINE_ANOTHER_FWK_MODULE(Histogrammer);
