b30ae4bbe83df8a293db5e6336ae2453e517ccc1
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2009-09-24 10:22:42 +0000

    More consistency between skim condition and output event content (before, in some cases the muons triggering the skim could fail the conditions in the output and not be saved...)

--- test/skim/skimJPsi_cfg.py
+++ test/skim/skimJPsi_cfg.py
@@ -23,13 +23,36 @@ massRangeSta = (2.0, 5.0)
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.source = cms.Source("PoolSource",  
     fileNames = cms.untracked.vstring(
-        'file:/afs/cern.ch/user/g/gpetrucc/scratch0/huntForRedOctober/CMSSW_3_1_2/src/MuonAnalysis/TagAndProbe/test/crab/ppMuX_AODSIM.root'
+        #'file:/afs/cern.ch/user/g/gpetrucc/scratch0/huntForRedOctober/CMSSW_3_1_2/src/MuonAnalysis/TagAndProbe/test/crab/ppMuX_AODSIM.root'
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0042/F41E5C99-A98B-DE11-B023-001D0967BE87.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0042/54BB83B5-8C8B-DE11-A580-001D0967E07F.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/FC3D5957-8B8B-DE11-9D92-0019B9E4878A.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/EA16CB89-6B8B-DE11-93CB-001D0967DC7E.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/D41FAF77-748B-DE11-9212-001D0968F5BC.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/C0F0B48E-8B8B-DE11-9568-001D0967DC7E.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/BE5592CF-6E8B-DE11-8DDD-001D0967A19D.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/BA582338-8A8B-DE11-82BE-001D0967E07F.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/B247689E-6B8B-DE11-83C6-0015178C15DC.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/A4088A9A-8B8B-DE11-9FA7-0019B9E48C13.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/821CCF6D-6D8B-DE11-A192-0015179ECB84.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/80CEE1B5-8A8B-DE11-8237-001D096B0BDE.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/7EAACE2B-8A8B-DE11-8229-001D0967DC4C.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/545ABAB4-8A8B-DE11-9F0E-001D0967DA3A.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/26B431BF-698B-DE11-9719-001D0967D39B.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0041/0AC6C38A-6B8B-DE11-855A-001D0967D341.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0040/E08D7559-398B-DE11-916A-0015178C4B94.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0040/E08660B6-488B-DE11-BE7E-001D0967C987.root',
+	'/store/mc/Summer09/JPsiMuMu/AODSIM/MC_31X_V3_AODSIM-v1/0040/D6366A95-558B-DE11-AA1B-0015178C6478.root',
     )
 )
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+## === GlobalTag is needed to read L1 infomration ====
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = "MC_31X_V3::All"
+
 ##     ____  _ _                     _                  _    ___  ____  
 ##    / ___|| (_)_ __ ___  _ __ ___ (_)_ __   __ _     / \  / _ \|  _ \ 
 ##    \___ \| | | '_ ` _ \| '_ ` _ \| | '_ \ / _` |   / _ \| | | | | | |
@@ -53,7 +76,7 @@ process.goodTracks = cms.EDFilter("TrackSelector",
 
 process.slimAOD = cms.Sequence(
     process.genMuons +
-    process.goodTracks
+    process.goodTracks 
 )
 
 
@@ -112,7 +135,7 @@ process.patMuonsWithTrigger = cms.EDProducer( "PATTriggerMatchMuonEmbedder",
 ### ==== Select ====
 process.patMuons = cms.EDFilter("PATMuonSelector",
     src = cms.InputTag("patMuonsWithTrigger"),
-    cut = cms.string("pt > %f" % (ptMin,)),
+    cut = cms.string("pt > %f" % (ptMin,)), 
 )
 ### ==== Sequence ====
 process.patMuonSequence = cms.Sequence( 
@@ -140,8 +163,8 @@ process.staTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
     particleType = cms.string("mu+"),
 )
 ## ==== Muons that are not only standalone (for skimming only) ====
-process.nonStaOnlyMuon = cms.EDFilter("MuonRefSelector",
-    src = cms.InputTag("muons"),
+process.nonStaOnlyMuon = cms.EDFilter("PATMuonRefSelector",
+    src = cms.InputTag("patMuons"),
     cut = cms.string("track.isNonnull"),
 )
 
@@ -216,14 +239,32 @@ process.Skim_jpsiMu  = cms.Path(process.jpsiMu  * process.jpsiMuFilter )
 process.Skim_jpsiTk  = cms.Path(process.jpsiTk  * process.jpsiTkFilter )
 process.Skim_jpsiSta = cms.Path(process.jpsiSta * process.jpsiStaFilter)
 
-# I define also a path that just skims on the trigger bit, to see what's the efficiency.
-# it won't be enabled in the PoolOutputModule
+# I define a few other paths that are not enabled in the OutputModule but which are useful to see what is the
+# efficiency of the various steps
+# to run them and see the efficiency, uncomment them in the cms.Schedule
 from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
 process.filterHLTMuX = hltHighLevel.clone(
     HLTPaths = [ triggerPath  ],
     TriggerResultsTag = cms.InputTag("TriggerResults","",triggerProcess),
 )
-process.Skim_HLT_MuX  = cms.Path(process.filterHLTMuX)
+process.oneTagFilter  = process.jpsiMuFilter.clone(src = 'goldenMuons')
+process.acceptanceFilter = cms.EDFilter("CandViewRefSelector",
+    src = cms.InputTag("genMuons"),
+    cut = cms.string(("pdgId == 443 && abs(daughter(0).pdgId) == 13 && ")+
+                     ("abs(daughter(0).eta) < %f && abs(daughter(1).eta) < %f && " % (etaTag, etaTag))+
+                     ("daughter(0).pt > %f && daughter(1).pt > %f && max(daughter(0).pt, daughter(1).pt) > %f" % (ptMin, ptMin, ptTag)) ),
+    filter = cms.bool(True),
+)
+process.Check_OneTag  = cms.Path(process.filterHLTMuX * process.oneTagFilter)
+# i clone these, as now they'll have different filters before...
+process.filterHLTAcc  = process.filterHLTMuX.clone()
+process.oneTagFilterAcc = process.oneTagFilter.clone()
+process.jpsiMuFilterAcc = process.jpsiMuFilter.clone()
+process.jpsiTkFilterAcc = process.jpsiTkFilter.clone()
+process.jpsiStaFilterAcc = process.jpsiStaFilter.clone()
+process.Check_Acceptance_Mu  = cms.Path(process.acceptanceFilter * process.filterHLTAcc * process.oneTagFilterAcc * process.jpsiMuFilterAcc)
+process.Check_Acceptance_Tk  = cms.Path(process.acceptanceFilter * process.filterHLTAcc * process.oneTagFilterAcc * process.jpsiTkFilterAcc)
+process.Check_Acceptance_Sta = cms.Path(process.acceptanceFilter * process.filterHLTAcc * process.oneTagFilterAcc * process.jpsiStaFilterAcc)
 
 
 ##     ___        _               _   
@@ -240,9 +281,11 @@ process.out = cms.OutputModule("PoolOutputModule",
         "keep *_patMuons_*_Skim",
         "keep *_goodTracks_*_Skim",
         "keep *_genMuons_*_Skim",
-        "keep recoTrackExtras_standAloneMuons_*_*", ## track states at the muon system
+        "keep recoTrackExtras_standAloneMuons_*_*",       ## track states at the muon system, used both by patMuons and standAloneMuons
+        "keep recoTracks_standAloneMuons_UpdatedAtVtx_*", ## bare standalone muon tracks, using standalone muon momentum (with BS constraint)
         "keep edmTriggerResults_*_*_Skim",          ## to know which kind of skim channel got us the event   
-        "keep l1extraL1MuonParticles_hltL1extraParticles_*_*", ## if we ever want to do L1 efficiency too
+       #"keep l1extraL1MuonParticles_l1extraParticles_*_*",  ## if we ever want to do L1 efficiency too ## <<--- Not in 3.1.X AODSIM
+       # "keep *_jpsiMu_*_Skim", "keep *_jpsiTk_*_Skim", "keep *_jpsiSta_*_Skim",                       ## <<--- keep these for monitoring
     ),
     SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring(
         "Skim_jpsiMu",
@@ -266,8 +309,11 @@ process.schedule = cms.Schedule(
     process.Skim_jpsiMu,
     process.Skim_jpsiTk,
     process.Skim_jpsiSta,
-    process.Skim_HLT_MuX, 
+    process.Check_OneTag, 
+    process.Check_Acceptance_Mu, 
+    process.Check_Acceptance_Tk, 
+    process.Check_Acceptance_Sta, 
     process.end
 )
 #process.out.fileName = "/tmp/gpetrucc/skimJPsi.root"
-#process.maxEvents.input = 5000
+process.maxEvents.input = 10000
