824e9ba8c32b4cb98c6b32149fe52927b93af1df
Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch>
2009-10-08 09:22:57 +0000

    Add a script for making dropbox-compatible DB and metadata files.

---
+++ test/performanceDB/MakeDropboxMetaDataFiles.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import os, string, sys, posix, tokenize, array, getopt
+
+def main(argv):
+
+    tables = ['GLBMUJPSI_OCTXTEST_TABLE','TRKEFFMUJPSI_OCTXTEST_TABLE','TRGMUJPSI_OCTXTEST_TABLE','GLBMUJPSICAL_OCTXTEST_TABLE',
+              'GLBMUZ_OCTXTEST_TABLE','TRKEFFMUZ_OCTXTEST_TABLE','TRGMUZ_OCTXTEST_TABLE','GLBMUZCAL_OCTXTEST_TABLE']
+
+    wps = ['GLBMUJPSI_OCTXTEST_WP','TRKEFFMUJPSI_OCTXTEST_WP','TRGMUJPSI_OCTXTEST_WP','GLBMUJPSICAL_OCTXTEST_WP',
+           'GLBMUZ_OCTXTEST_WP','TRKEFFMUZ_OCTXTEST_WP','TRGMUZ_OCTXTEST_WP','GLBMUZCAL_OCTXTEST_WP']
+    
+    effs = ['GlobalMuonFromTrackerTrackJpsi',
+            'TrackerTrackFromStandaloneMuonJpsi',
+            'TriggerMuonFromGlobalMuonJpsi',
+            'GlobalMuonFromCaloMuonJpsi',
+            'GlobalMuonFromTrackerTrackZ',
+            'TrackerTrackFromStandaloneMuonZ',
+            'TriggerMuonFromGlobalMuonZ',
+            'GlobalMuonFromCaloMuonZ'
+            ]
+    
+    curdir = '/tmp/jjhollar'
+    files = os.listdir(curdir)
+
+    dbfilename = 'MuonPhysicsPerformance.db'
+
+    i = 0
+    for table in tables:
+        eff = effs[i]
+        metadataprefixname = 'TNPEff_' + str(eff) + '_Table'
+        metadatafilename = 'TNPEff_' + str(eff) + '_Table.txt'
+        newfile = open(metadatafilename, 'w')
+        newfile.write('destDB oracle://cms_orcoff_prep/CMS_COND_PHYSICSTOOLS\n')
+        newfile.write('inputtag\n')
+        newfile.write('tag ' + table + '\n')
+        newfile.write('since\n')
+        newfile.write('till\n')
+        newfile.write('usertext First test of October exercise Tag&Probe efficiencies ' + eff + ' table\n')
+        newfile.close()
+        cmd = "uuidgen -t"
+        fin,fout = os.popen4(cmd)
+        uuid = str(fout.read()).rstrip()
+
+        movedfilename = metadataprefixname + '@' + uuid + '.txt'
+        movecommand = 'mv ' + metadatafilename + ' ' + movedfilename
+        os.system(movecommand)
+
+        copydbfilename = metadataprefixname + '@' + uuid + '.db'
+        copycommand = 'cp ' + dbfilename + ' ' + copydbfilename
+        os.system(copycommand)
+        print movedfilename
+        print copydbfilename
+        i = i+1
+        
+    i = 0
+    for wp in wps:
+        eff = effs[i]
+        metadataprefixname = 'TNPEff_' + str(eff) + '_WP'                 
+        metadatafilename = 'TNPEff_' + str(eff) + '_WP.txt'
+        newfile = open(metadatafilename, 'w')
+        newfile.write('destDB oracle://cms_orcoff_prep/CMS_COND_PHYSICSTOOLS\n')
+        newfile.write('inputtag\n')
+        newfile.write('tag ' + wp + '\n')
+        newfile.write('since\n')
+        newfile.write('till\n')
+        newfile.write('usertext First test of October exercise Tag&Probe efficiencies ' + eff + ' working point\n')
+        newfile.close()                                                                
+        cmd = "uuidgen -t"
+        fin,fout = os.popen4(cmd)
+        uuid = str(fout.read()).rstrip()
+
+        movedfilename = metadataprefixname + '@' + uuid + '.txt'
+        movecommand = 'mv ' + metadatafilename + ' ' + movedfilename
+        os.system(movecommand)
+
+        copydbfilename = metadataprefixname + '@' + uuid + '.db'
+        copycommand = 'cp ' + dbfilename + ' ' + copydbfilename
+        os.system(copycommand)
+        print movedfilename
+        print copydbfilename
+        i = i+1
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
