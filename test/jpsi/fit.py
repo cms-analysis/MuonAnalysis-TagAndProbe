42556f1fc3ffc605529babcf3050311f4b473a5f
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2010-02-22 15:04:22 +0000

    Updated to the new fitting code (first attempt)

--- test/jpsi/fit.py
+++ test/jpsi/fit.py
@@ -3,106 +3,191 @@ process = cms.Process("Fit")
 
 ## Note: mass ranges for the fit don't have to be the same as the mass ranges for the 
 ##       making of T&P pairs  in tp_from_skim.py
-massRange = ( 2.8, 3.5 ); massRangeSta = (2, 5)
+massRange = ( 2.8, 3.5 ); massRangeSta = (2, 4.5)
 
 # Add your own files here
 process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )    
 
-RunFit = cms.EDAnalyzer("TagProbeEDMAnalysis",  
-      ## Efficiency/Fitting variables
-      CalculateEffSideBand = cms.untracked.bool( True ), ## Calculate and store effs using SB
-      CalculateEffFitter   = cms.untracked.bool( True ), ## Calculate and store effs from Roofit
-      CalculateEffTruth    = cms.untracked.bool( True ), ## Calculate and store true effs
+def defineEfficiencies(Module, ptBins=None, etaBins=None, passing="passing", model=None, mc=False):
+    Module.Categories = cms.untracked.PSet()
+    setattr(Module.Categories, passing, cms.untracked.vstring(passing, "dummy[pass=1,fail=0]"))
+    if mc:
+        Module.Categories.mcTrue = cms.untracked.vstring("MC true", "dummy[true=1,false=0]")
+    # defines a set of efficiency calculations, what PDF to use for fitting and how to bin the data;
+    # there will be a separate output directory for each calculation that includes a simultaneous fit, side band subtraction and counting. 
+    Module.Efficiencies = cms.untracked.PSet(
+        #the name of the parameter set becomes the name of the directory
+        pt = cms.untracked.PSet(
+            #specifies the efficiency of which category and state to measure 
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            #specifies what unbinned variables to include in the dataset, the mass is needed for the fit
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            #specifies the binning of parameters
+            BinnedVariables = cms.untracked.PSet(pt = ptBins),
+            #first string is the default followed by binRegExp - PDFname pairs
+            BinToPDFmap = cms.untracked.vstring(model)
+        ),
+        eta = cms.untracked.PSet(
+            #specifies the efficiency of which category and state to measure 
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            #specifies what unbinned variables to include in the dataset, the mass is needed for the fit
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            #specifies the binning of parameters
+            BinnedVariables = cms.untracked.PSet(eta = etaBins),
+            #first string is the default followed by binRegExp - PDFname pairs
+            BinToPDFmap = cms.untracked.vstring(model)
+        ),
 
-      ## Set mode to read from files ...
-      Mode = cms.untracked.string("ReadNew"),
-
-      ReadFromFiles = cms.untracked.vstring("tnpJPsi.root"),
-      ReadFromDirectory = cms.untracked.string("TO_BE_FILLED"),
-      FitFileName       = cms.untracked.string("fit_output.root"), ##<< actually not used
-
-      UnbinnedFit  = cms.untracked.bool( True ),
-      Do2DFit      = cms.untracked.bool( True ),
-      HasWeights   = cms.untracked.bool( False ), ## <<< Specify there are no weights
-
-      ## Mass window for fitting
-      NumBinsMass         = cms.untracked.int32( 20 ), # matters only for the plots, the fit is unbinned
-      MassLow             = cms.untracked.double( massRange[0] ),
-      MassHigh            = cms.untracked.double( massRange[1] ),
+        pt_eta = cms.untracked.PSet(
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            BinnedVariables = cms.untracked.PSet(
+                pt = ptBins,
+                eta = etaBins,
+            ),
+            BinToPDFmap = cms.untracked.vstring(model)
+        ),
+    )
+    if mc:
+        Module.Efficiencies.pt_mcTrue = cms.untracked.PSet(
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            BinnedVariables = cms.untracked.PSet(
+                mcTrue = cms.untracked.vstring("true"),
+                pt = ptBins,
+            )
+            #unspecified binToPDFmap means no fitting
+        )
+        Module.Efficiencies.eta_mcTrue = cms.untracked.PSet(
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            BinnedVariables = cms.untracked.PSet(
+                mcTrue = cms.untracked.vstring("true"),
+                eta = etaBins, 
+            )
+            #unspecified binToPDFmap means no fitting
+        )
+        Module.Efficiencies.pt_eta_mcTrue = cms.untracked.PSet(
+            EfficiencyCategoryAndState = cms.untracked.vstring(passing,"pass"),
+            UnbinnedVariables = cms.untracked.vstring("mass"),
+            BinnedVariables = cms.untracked.PSet(
+                mcTrue = cms.untracked.vstring("true"),
+                pt = ptBins,
+                eta = etaBins,
+            )
+        )
     
-      ## Variable Specifications for SB subtractions and Roofit
-      NameVar1             = cms.untracked.string( "pt" ),
-      Var1BinBoundaries   = cms.untracked.vdouble( 1.5, 3, 4.5, 6, 10, 20),
-      NameVar2             = cms.untracked.string( "eta" ),
-      Var2BinBoundaries   = cms.untracked.vdouble( -2.4, -1.3, -0.8, 0.8, 1.3, 2.4),
 
-      PassingProbeName = cms.untracked.string("TO_BE_FILLED"), # <<< You have to specify the column that says if a probe passed or not
+RunFit  = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
+    # IO parameters:
+    InputFileName = cms.untracked.string("tnpJPsi.root"),
+    InputTreeName = cms.untracked.string("fitter_tree"),
+    #numbrer of CPUs to use for fitting
+    NumCPU = cms.untracked.uint32(4),
+    # specifies wether to save the RooWorkspace containing the data for each bin and
+    # the pdf object with the initial and final state snapshots
+    SaveWorkspace = cms.untracked.bool(False),
 
-      GaussLineShape = cms.untracked.PSet(
-	GaussMean        = cms.untracked.vdouble( 3.09, 2.9,  3.1  ),
-	GaussSigma       = cms.untracked.vdouble( 0.03, 0.01, 0.05 ),
-      ),
+    # defines all the real variables of the probes available in the input tree and intended for use in the efficiencies
+    Variables = cms.untracked.PSet(
+        mass = cms.untracked.vstring("Tag-Probe Mass", str(massRange[0]), str(massRange[1]), "GeV/c^{2}"),
+        pt = cms.untracked.vstring("Probe p_{T}", "0", "1000", "GeV/c"),
+        eta = cms.untracked.vstring("Probe #eta", "-2.4", "2.4", "")
+    ),
 
-      ## Background variables
-      CMSBkgLineShape = cms.untracked.PSet(
-	CMSBkgAlpha           = cms.untracked.vdouble( 0 ), # fix these two to zero
-	CMSBkgBeta            = cms.untracked.vdouble( 0 ), # so it's just an exp
-	CMSBkgPeak            = cms.untracked.vdouble( 3.09 ),
-	CMSBkgGamma           = cms.untracked.vdouble( 0, -10, 10 )
-      ),
+    # defines all the discrete variables of the probes available in the input tree and intended for use in the efficiency calculations
+    Categories = cms.untracked.PSet(
+        mcTrue = cms.untracked.vstring("MC true", "dummy[true=1,false=0]"),
+        passing = cms.untracked.vstring("isMuon", "dummy[pass=1,fail=0]")
+    ),
 
-      ## Efficiency variables
-      Efficiency        = cms.untracked.vdouble( 0.90,0.0,1.0 ),    
-      NumSignal         = cms.untracked.vdouble( 4000.0,-10.0,30000.0 ),    
-      NumBkgPass        = cms.untracked.vdouble( 4000.0,-10.0,10000.0 ),    
-      NumBkgFail        = cms.untracked.vdouble( 1000.0,-10.0,7000.0  ),    
+    # defines all the PDFs that will be available for the efficiency calculations; uses RooFit's "factory" syntax;
+    # each pdf needs to define "signal", "backgroundPass", "backgroundFail" pdfs, "efficiency[0.9,0,1]" and "signalFractionInPassing[0.9]" are used for initial values  
+    PDFs = cms.untracked.PSet(
+        gaussPlusFloatExpo = cms.untracked.vstring(
+            "Gaussian::signal(mass, mean[3.09,2.9,3.2], sigma[0.03,0.01,0.05])",
+            "Exponential::backgroundPass(mass, slopePass[0,-10,10])",
+            "Exponential::backgroundFail(mass, slopeFail[0,-10,10])",
+            "efficiency[0.9,0,1]",
+            "signalFractionInPassing[0.9]"
+        ),
+        gaussPlusChebychev3 = cms.untracked.vstring(
+            "Gaussian::signal(mass, mean[3.09,2.9,3.2], sigma[0.15,0.05,1.0])",
+            "Chebychev::backgroundPass(mass, {c1[0,-1,1], c2[0,-1,1], c3[0,-1,1]})",
+            "Chebychev::backgroundFail(mass, {c1, c2, c3})",
+            "efficiency[0.9,0,1]",
+            "signalFractionInPassing[0.9]"
+        )
+    ),
 
-      ## Variables for sideband subtraction
-      SBSPeak            = cms.untracked.double( 3.09 ),   ## Mass peak
-      SBSStanDev         = cms.untracked.double( 0.03 ),   ## SD from peak for subtraction
-
-      # All the following is useless now that we just read and fit
-      # but it's required...
-      # --------------------------------------------
-      MCTruthParentId = cms.untracked.int32(443),
-)
+    # defines a set of efficiency calculations, what PDF to use for fitting and how to bin the data;
+    # there will be a separate output directory for each calculation that includes a simultaneous fit, side band subtraction and counting. 
+ )
 
 
 process.fitGlbFromTk = RunFit.clone(
-    ReadFromDirectory = 'histoMuFromTk',
-    PassingProbeName  = 'passingGlb',
+    InputDirectoryName = cms.untracked.string('histoMuFromTk'),
+    OutputFileName     = cms.untracked.string('fitGlbFromTk.root'),
 )
-process.fitGlbFromCal = RunFit.clone(
-    ReadFromDirectory = 'histoMuFromCal',
-    PassingProbeName  = 'passingGlb',
+defineEfficiencies(process.fitGlbFromTk,
+    ptBins  = cms.untracked.vdouble( 1.5, 3, 4.5, 6, 10, 20),
+    etaBins = cms.untracked.vdouble( -2.4, -1.3, -0.8, 0.8, 1.3, 2.4),
+    passing = 'passingGlb',
+    model = 'gaussPlusFloatExpo',
+    mc = True
 )
-process.fitTrkExFromTk  = process.fitGlbFromTk.clone( PassingProbeName  = 'passingTrkEx')
-process.fitTrkExFromCal = process.fitGlbFromCal.clone(PassingProbeName  = 'passingTrkEx')
+
+process.fitGlbFromCal = process.fitGlbFromTk.clone(
+    InputDirectoryName = 'histoMuFromCal',
+    OutputFileName     = 'fitGlbFromCal.root',
+)
+process.fitTrkExFromTk  = RunFit.clone(
+    InputDirectoryName = cms.untracked.string('histoMuFromTk'),
+    OutputFileName     = cms.untracked.string('fitTrkExFromTk.root'),
+)
+defineEfficiencies(process.fitTrkExFromTk,
+    ptBins  = cms.untracked.vdouble( 1.5, 3, 4.5, 6, 10, 20),
+    etaBins = cms.untracked.vdouble( -2.4, -1.3, -0.8, 0.8, 1.3, 2.4),
+    passing = 'passingTrkEx',
+    model = 'gaussPlusFloatExpo',
+    mc = True
+)
+process.fitTrkExFromCal = process.fitTrkExFromTk.clone(
+    InputDirectoryName = 'histoMuFromCal',
+    OutputFileName     = 'fitTrkExFromCal.root' 
+)
 
 process.fitTkFromSta = RunFit.clone(
-    ReadFromDirectory = 'histoTkFromSta',
-    PassingProbeName  = 'passing',
-    ## Override mass window and line shape for Sta muons
-    MassLow  = cms.untracked.double( massRangeSta[0] ),
-    MassHigh = cms.untracked.double( massRangeSta[1] ),
-    GaussLineShape = cms.untracked.PSet(
-        GaussMean  = cms.untracked.vdouble( 3.09, 2.9,  3.1 ),
-        GaussSigma = cms.untracked.vdouble( 0.15, 0.05, 1.0 ),
-    ),
+    InputDirectoryName = cms.untracked.string('histoTkFromSta'),
+    OutputFileName     = cms.untracked.string('fitTkFromSta.root'),
 )
+defineEfficiencies(process.fitTkFromSta,
+    ptBins  = cms.untracked.vdouble( 1.5, 3, 4.5, 6, 10, 20),
+    etaBins = cms.untracked.vdouble( -2.4, -1.3, -0.8, 0.8, 1.3, 2.4),
+    passing = 'passing',
+    model = 'gaussPlusChebychev3',
+    mc = True
+)
+process.fitTkFromSta.Variables.mass = cms.untracked.vstring("Tag-Probe Mass", str(massRangeSta[0]), str(massRangeSta[1]), "GeV/c^{2}")
 
-process.fitMu3FromGlb = RunFit.clone(
-    ReadFromDirectory = 'histoHltFromGlb',
-    PassingProbeName  = 'Mu3',
-    ## Override pt. eta bins for trigger
-    Var1BinBoundaries   = cms.untracked.vdouble( 3, 4.5, 6, 10, 20),
-    Var2BinBoundaries   = cms.untracked.vdouble( -2.1,-1.2,-0.7,0.0,0.7,1.2,2.1),
-)
-process.fitMu5FromGlb    = process.fitMu3FromGlb.clone(PassingProbeName  = 'Mu5')
-process.fitDiMu0FromGlb  = process.fitMu3FromGlb.clone(PassingProbeName  = 'DoubleMu0')
-process.fitDiMu3FromGlb  = process.fitMu3FromGlb.clone(PassingProbeName  = 'DoubleMu3')
-process.fitL1MuOpFromGlb = process.fitMu3FromGlb.clone(PassingProbeName  = 'L1MuOpen')
+for L in [ 'Mu3', 'Mu5', 'DoubleMu0', 'DoubleMu3', 'L1MuOpen' ]:
+    label = "fit%sFromGlb" % (L,);
+    setattr(process,
+        label,
+        RunFit.clone(
+            InputDirectoryName = cms.untracked.string('histoHltFromGlb'),
+            OutputFileName     = cms.untracked.string(label+'.root'),
+        )
+    )
+    getattr(process,label).Variables.eta = cms.untracked.vstring("Probe #eta", "-2.1", "2.1", "")
+    defineEfficiencies(getattr(process,label),
+        ptBins  = cms.untracked.vdouble( 1.5, 3, 4.5, 6, 10, 20),
+        etaBins = cms.untracked.vdouble( -2.1,-1.2,-0.7,0.0,0.7,1.2,2.1),
+        passing = L,
+        model = 'gaussPlusFloatExpo',
+        mc = True
+    )
 
 process.fitness = cms.Path(
     process.fitGlbFromTk +
@@ -112,9 +197,9 @@ process.fitness = cms.Path(
     process.fitTkFromSta +
     process.fitMu3FromGlb +
     process.fitMu5FromGlb +
-    process.fitDiMu0FromGlb +
-    process.fitDiMu3FromGlb +
-    process.fitL1MuOpFromGlb 
+    process.fitDoubleMu0FromGlb +
+    process.fitDoubleMu3FromGlb +
+    process.fitL1MuOpenFromGlb 
 )
 
-process.TFileService = cms.Service("TFileService", fileName = cms.string("fitJPsi.root"))
+#process.TFileService = cms.Service("TFileService", fileName = cms.string("fitJPsi.root"))
