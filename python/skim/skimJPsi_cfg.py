ea029a8c2b65f07ead24ee856c90f1f3c708c486
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2010-03-01 15:16:16 +0000

    Adapt to the 1mu-2mu triggers, but stay compatible with Summer09/3.1.X

--- python/skim/skimJPsi_cfg.py
+++ python/skim/skimJPsi_cfg.py
@@ -9,14 +9,15 @@ process = cms.Process("Skim")
 ##    \____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|_.__/|_|\___||___/
 ##                           |___/                                   
 ##   
-ptMin  = 1.5; # minimum pT to consider tracks and muons
-ptTag  = 3.0; # minimum pT for the tag
-etaTag = 2.5; # maximum |eta| for the tag
-#triggerProcess = 'HLT8E29'  # when running on 3.1.X MC
-triggerProcess = 'HLT'       # when  running on 3.3.X MC or RelVals
-triggerPath    = 'HLT_Mu3'   # Trigger name
-massRangeMu  = (2.0, 4.0)
-massRangeTk  = (2.0, 4.0) 
+ptMinCut       = 'pt > 2 || (abs(eta) > 1 && p > 2)';
+triggerProcess = 'HLT8E29'  # when running on 3.1.X MC
+#triggerProcess = 'HLT'       # when  running on 3.3.X MC or RelVals
+triggerPath1Mu = 'HLT_Mu3'                 # Single-muon Trigger name
+triggerFilt1Mu = 'hltSingleMu3L3Filtered3' # Single-muon Trigger last filter name
+triggerPath2Mu = 'HLT_L1DoubleMuOpen'                  # Double-muon Trigger name
+triggerFilt2Mu = 'hltDoubleMuLevel1PathL1OpenFiltered' # Double-muon Trigger name
+massRangeMu  = (2.6, 3.6)
+massRangeTk  = (2.6, 3.6) 
 massRangeSta = (2.0, 5.0)
 
 
@@ -26,17 +27,24 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.source = cms.Source("PoolSource",  
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/DC4D9E50-82ED-DE11-BF03-003048D375AA.root',
-        '/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/88F9B6DC-85ED-DE11-A295-001D09F2514F.root',
-        '/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/66B9D232-85ED-DE11-B5EC-0019B9F7312C.root',
-        '/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/509FE9BB-83ED-DE11-A25F-001D09F253C0.root',
-        '/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/4294CBAC-B5ED-DE11-B7E8-003048D2BE08.root',
+        #'/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/DC4D9E50-82ED-DE11-BF03-003048D375AA.root',
+        #'/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/88F9B6DC-85ED-DE11-A295-001D09F2514F.root',
+        #'/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/66B9D232-85ED-DE11-B5EC-0019B9F7312C.root',
+        #'/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/509FE9BB-83ED-DE11-A25F-001D09F253C0.root',
+        #'/store/relval/CMSSW_3_4_1/RelValJpsiMM/GEN-SIM-RECO/STARTUP3X_V14-v1/0004/4294CBAC-B5ED-DE11-B7E8-003048D2BE08.root',
+        #'file:/data/mc/7TeV/Summer09/JPsiMuMu_AODSIM_72B0D83F-908B-DE11-9A88-001D09646131.root'
+         'file:/data/mc/7TeV/Summer09/ppMuX_AODSIM_EAB08D8C-78A6-DE11-88FD-001AA009562B.root'
     )
 )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-## === GlobalTag is needed to read L1 infomration ====
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = "STARTUP3X_V15::All"
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5000))
+## === conditions are needed to access geometry for L1 matching ====
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.GeometryExtended_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.GlobalTag.globaltag = "MC_3XY_V18::All"  # 3.1.X MC is Ideal
+#process.GlobalTag.globaltag = "START3X_V18::All"
+
 
 ##     ____  _ _                     _                  _    ___  ____  
 ##    / ___|| (_)_ __ ___  _ __ ___ (_)_ __   __ _     / \  / _ \|  _ \ 
@@ -56,7 +64,7 @@ process.genMuons = cms.EDProducer("GenParticlePruner",
 ## ==== Just ignore all the too low pt stuff ====
 process.goodTracks = cms.EDFilter("TrackSelector",
     src = cms.InputTag("generalTracks"),
-    cut = cms.string("pt > %f" % (ptMin,) ),
+    cut = cms.string(ptMinCut),
 )
 
 process.slimAOD = cms.Sequence(
@@ -75,8 +83,10 @@ process.slimAOD = cms.Sequence(
 ##  We take them from Onia2MuMuPAT but we make a few changes
 ##
 process.load("HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cff");
+## Trigger matches
+process.patTrigger.processName = triggerProcess
 ## put a PT cut on the muons
-process.patMuons.cut = "pt > %f" % (ptMin,);
+process.patMuons.cut = ptMinCut;
 ## use the genMuons as MC source, so that we keep them and have the correct mother refs
 process.muonMatch.matched = 'genMuons'
 ## switch off genParticle embedding, as we keep the genMuons collection
@@ -109,19 +119,25 @@ process.nonStaOnlyMuon = cms.EDFilter("PATMuonRefSelector",
 )
 
 ## ==== Golden muons to be used for tags. use PAT ones, so I can check HLT =====
-PASS_HLT = "!triggerObjectMatchesByPath('%s').empty()" % (triggerPath,);
-process.goldenMuons = cms.EDFilter("PATMuonRefSelector",
+PASS_HLT_1 = "!triggerObjectMatchesByFilter('%s').empty()" % (triggerFilt1Mu,);
+PASS_HLT_2 = "!triggerObjectMatchesByFilter('%s').empty()" % (triggerFilt2Mu,);
+process.tags1Mu = cms.EDFilter("PATMuonRefSelector",
     src = cms.InputTag("patMuons"),
-    cut = cms.string("isGlobalMuon && pt > %f && abs(eta) < %f && %s" % (ptTag, etaTag, PASS_HLT) ), 
+    cut = cms.string("isGlobalMuon && "+ PASS_HLT_1), 
 )
+process.tags2Mu = cms.EDFilter("PATMuonRefSelector",
+    src = cms.InputTag("patMuons"),
+    cut = cms.string("isGlobalMuon && " + PASS_HLT_2), 
+)
+
 
 process.otherMuons = cms.Sequence(
     process.nonStaOnlyMuon +
-    process.goldenMuons +
+    process.tags1Mu +
+    process.tags2Mu +
     process.tkTracks +
     process.staTracks
 )
-
 
 process.allMuons = cms.Sequence(
     process.patMuonSequence *
@@ -136,15 +152,15 @@ process.allMuons = cms.Sequence(
 ##                                                        
 ##   
 process.jpsiMu  = cms.EDProducer("CandViewShallowCloneCombiner",
-    decay = cms.string("goldenMuons@+ nonStaOnlyMuon@-"),
+    decay = cms.string("tags1Mu@+ nonStaOnlyMuon@-"),
     cut = cms.string("%f < mass < %f" % massRangeMu),
 )
 process.jpsiTk  = cms.EDProducer("CandViewShallowCloneCombiner",
-    decay = cms.string("goldenMuons@+ tkTracks@-"),
+    decay = cms.string("tags1Mu@+ tkTracks@-"),
     cut = cms.string("%f < mass < %f" % massRangeTk),
 )
 process.jpsiSta = cms.EDProducer("CandViewShallowCloneCombiner",
-    decay = cms.string("goldenMuons@+ staTracks@-"),
+    decay = cms.string("tags2Mu@+ staTracks@-"),
     cut = cms.string("%f < mass < %f" % massRangeSta),
 )
 process.allJPsis = cms.Sequence(
@@ -218,22 +234,33 @@ process.jpsiMuFilter  = cms.EDFilter("CandViewCountFilter",
 process.jpsiTkFilter  = process.jpsiMuFilter.clone(src = 'jpsiTk')
 process.jpsiStaFilter = process.jpsiMuFilter.clone(src = 'jpsiSta')
 
-process.Skim_jpsiMu  = cms.Path(process.collisionFilter + process.jpsiMuFilter )
-process.Skim_jpsiTk  = cms.Path(process.collisionFilter + process.jpsiTkFilter )
-process.Skim_jpsiSta = cms.Path(process.collisionFilter + process.jpsiStaFilter)
+## Note: we don't use the collision filter when running on 'high luminosity' MC
+#process.Skim_jpsiMu  = cms.Path(process.collisionFilter + process.jpsiMuFilter )
+#process.Skim_jpsiTk  = cms.Path(process.collisionFilter + process.jpsiTkFilter )
+#process.Skim_jpsiSta = cms.Path(process.collisionFilter + process.jpsiStaFilter)
+process.Skim_jpsiMu  = cms.Path(process.jpsiMuFilter )
+process.Skim_jpsiTk  = cms.Path(process.jpsiTkFilter )
+process.Skim_jpsiSta = cms.Path(process.jpsiStaFilter)
 
 # I define a few other paths that are not enabled in the OutputModule but which are useful to see what is the
 # efficiency of the various steps
 # to run them and see the efficiency, uncomment them in the cms.Schedule
 from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
-process.filterHLTMuX = hltHighLevel.clone(
-    HLTPaths = [ triggerPath  ],
+process.filterHLT1Mu = hltHighLevel.clone(
+    HLTPaths = [ triggerPath1Mu  ],
     TriggerResultsTag = cms.InputTag("TriggerResults","",triggerProcess),
 )
-process.oneTagFilter  = process.jpsiMuFilter.clone(src = 'goldenMuons')
 
-process.Check_Collisions = cms.Path(process.collisionFilter)
-process.Check_OneTag     = cms.Path(process.collisionFilter + process.filterHLTMuX + process.oneTagFilter)
+process.filterHLT2Mu = process.filterHLT1Mu.clone(HLTPaths = [ triggerPath2Mu ])
+process.oneTag1MuFilter  = process.jpsiMuFilter.clone(src = 'tags1Mu')
+process.oneTag2MuFilter  = process.jpsiMuFilter.clone(src = 'tags2Mu')
+
+
+#process.Check_Collisions = cms.Path(process.collisionFilter)
+process.Check_HLT1Mu  = cms.Path(process.filterHLT1Mu)
+process.Check_HLT2Mu  = cms.Path(process.filterHLT2Mu)
+process.Check_OneTag1Mu  = cms.Path(process.oneTag1MuFilter)
+process.Check_OneTag2Mu  = cms.Path(process.oneTag2MuFilter)
 
 ##     ___        _               _   
 ##    / _ \ _   _| |_ _ __  _   _| |_ 
@@ -244,6 +271,7 @@ process.Check_OneTag     = cms.Path(process.collisionFilter + process.filterHLTM
 ##   
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string("skimJPsi.root"),
+    #fileName = cms.untracked.string("/tmp/gpetrucc/skimJPsi_ppMuX.root"),
     outputCommands = cms.untracked.vstring(
         "drop *",
         "keep *_patMuons_*_Skim",
@@ -252,10 +280,10 @@ process.out = cms.OutputModule("PoolOutputModule",
         "keep recoTrackExtras_standAloneMuons_*_*",          ## track states at the muon system, used both by patMuons and standAloneMuons
         "keep recoTracks_standAloneMuons_UpdatedAtVtx_*",    ## bare standalone muon tracks, using standalone muon momentum (with BS constraint)
         "keep edmTriggerResults_*_*_Skim",                   ## to know which kind of skim channel got us the event   
-        "keep l1extraL1MuonParticles_l1extraParticles_*_*",  ## if we ever want to do L1 efficiency too ## <<--- Not in 3.1.X AODSIM
-        "keep *_offlinePrimaryVertices__*",                  ## vertices and BS are not very useful on MC
-        "keep *_offlineBeamSpot__*",                         ## but they can be important on data
-       # "keep *_jpsiMu_*_Skim", "keep *_jpsiTk_*_Skim", "keep *_jpsiSta_*_Skim",                       ## <<--- keep these for monitoring
+       #"keep l1extraL1MuonParticles_l1extraParticles_*_*",  ## if we ever want to do L1 efficiency too ## <<--- Not in 3.1.X AODSIM
+       #"keep *_offlinePrimaryVertices__*",                  ## vertices and BS are not very useful on MC
+       #"keep *_offlineBeamSpot__*",                         ## but they can be important on data
+       #"keep *_jpsiMu_*_Skim", "keep *_jpsiTk_*_Skim", "keep *_jpsiSta_*_Skim",                       ## <<--- keep these for monitoring
     ),
     SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring(
         "Skim_jpsiMu",
@@ -277,8 +305,11 @@ process.schedule = cms.Schedule(
     process.Skim_jpsiMu,
     process.Skim_jpsiTk,
     process.Skim_jpsiSta,
-    process.Check_Collisions, 
-    process.Check_OneTag, 
+    #process.Check_Collisions, 
+    process.Check_OneTag1Mu, 
+    process.Check_OneTag2Mu, 
+    process.Check_HLT1Mu, 
+    process.Check_HLT2Mu, 
     process.end
 )
 
