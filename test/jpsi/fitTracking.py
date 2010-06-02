0d6de1d01e3d68fa9b0dff1c08fafd3b904fe36b
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2010-06-02 22:23:33 +0000

    Update cfg; only scenarios 'data_all' and 'datalike_mc' have been tested

--- test/jpsi/fitTracking.py
+++ test/jpsi/fitTracking.py
@@ -10,7 +10,7 @@ print "Will run scenario ", scenario
 
 CONSTRAINTS = cms.PSet(
     hasValidHits = cms.vstring("pass"),
-    tag_HLTMu3   = cms.vstring("pass"),
+    tag_Mu3   = cms.vstring("pass"),
 )
 PT_ETA_BINS = cms.PSet(
     CONSTRAINTS,
@@ -38,19 +38,22 @@ Template = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
     SaveWorkspace = cms.bool(False),
 
     Variables = cms.PSet(
-        mass = cms.vstring("Tag-Probe Mass", "2.0", "4.3", "GeV/c^{2}"),
-        pt = cms.vstring("Probe p_{T}", "0", "1000", "GeV/c"),
-        p = cms.vstring("Probe p", "0", "1000", "GeV/c"),
-        eta = cms.vstring("Probe #eta", "-2.5", "2.5", ""),
-        phi = cms.vstring("Probe #phi", "-3.1416", "3.1416", ""),
+        mass   = cms.vstring("Tag-Probe Mass", "2.0", "4.3", "GeV/c^{2}"),
+        pt     = cms.vstring("Probe p_{T}", "0", "1000", "GeV/c"),
+        p      = cms.vstring("Probe p", "0", "1000", "GeV/c"),
+        eta    = cms.vstring("Probe #eta", "-2.5", "2.5", ""),
+        abseta = cms.vstring("Probe |#eta|", "0", "2.5", ""),
+        phi    = cms.vstring("Probe #phi", "-3.1416", "3.1416", ""),
         tag_pt = cms.vstring("Tag p_{T}", "2.6", "1000", "GeV/c"),
     ),
 
     Categories = cms.PSet(
-        mcTrue = cms.vstring("MC true", "dummy[true=1,false=0]"),
-        passing = cms.vstring("passing", "dummy[pass=1,fail=0]"),
+        passing            = cms.vstring("passing",           "dummy[pass=1,fail=0]"),
+        passingNoJPsi      = cms.vstring("passingNoJPsi",     "dummy[pass=1,fail=0]"),
+        passingNoBestJPsi  = cms.vstring("passingNoBestJPsi", "dummy[pass=1,fail=0]"),
         hasValidHits = cms.vstring("hasValidHits",  "dummy[pass=1,fail=0]"),
-        tag_HLTMu3 = cms.vstring("tag_HLTMu3", "dummy[pass=1,fail=0]"),
+        tag_Mu3      = cms.vstring("tag_HLTMu3", "dummy[pass=1,fail=0]"),
+        mcTrue       = cms.vstring("MC true", "dummy[true=1,false=0]"),
     ),
 
     PDFs = cms.PSet(
@@ -60,7 +63,15 @@ Template = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
             "Chebychev::backgroundFail(mass, {c1,c2,c3})",
             "efficiency[0.9,0,1]",
             "signalFractionInPassing[0.9]"
+        ),
+        gaussPlusFloatCubic = cms.vstring(
+            "Gaussian::signal(mass, mean[3.1,3.0,3.2], sigma[0.2,0.05,0.5])",
+            "Chebychev::backgroundPass(mass, {c1p[0,-1,1], c2p[0,-1,1], c3p[0,-1,1]})",
+            "Chebychev::backgroundFail(mass, {c1f[0,-1,1], c2f[0,-1,1], c3f[0,-1,1]})",
+            "efficiency[0.9,0,1]",
+            "signalFractionInPassing[0.9]"
         )
+
     )
 )
 
@@ -109,33 +120,46 @@ elif scenario == "signal_0.5pb":
 if scenario.startswith("data"):
     if scenario == "data_all":
         process.TnP_Tracking.InputFileNames = [ "tnpJPsi_Data.root" ]
-    elif scenario == "data_0.001pb":
-         process.TnP_Tracking.InputFileNames = [ "/afs/cern.ch/user/g/gpetrucc/scratch0/tnp/tnpJPsi_Data_fromMay6th.root" ]
+        process.TnP_Tracking.binsForMassPlots = cms.uint32(23)
     elif scenario == "datalike_mc":
-         process.TnP_Tracking.InputFileNames = [ "tnpJPsi_JPsiMuMu_Spring10_0.03pb.root", "tnpJPsi_ppMuX_Spring10_0.03pb.root" ]
-    process.TnP_Tracking.Variables.tag_pt[1] = "0.0"; # don't cut on tag pt
-    process.TnP_Tracking.Efficiencies = cms.PSet(
-        eff = cms.PSet(
-            EfficiencyCategoryAndState = cms.vstring("passing","pass"),
+         process.TnP_Tracking.InputFileNames = [ "tnpJPsi_JPsiMuMu_Spring10_0.117pb.root", "tnpJPsi_ppMuX_Spring10_0.117pb.root" ]
+    #process.TnP_Tracking.Variables.tag_pt[1] = "0.0"; # don't cut on tag pt
+    process.TnP_Tracking.Efficiencies = cms.PSet()
+    sampleToPdfMap = { "": "gaussPlusCubic", "NoJPsi":"gaussPlusFloatCubic", "NoBestJPsi":"gaussPlusFloatCubic" }
+    for X in ("", "NoJPsi", "NoBestJPsi"):
+        setattr(process.TnP_Tracking.Efficiencies, "eff"+X, cms.PSet(
+            EfficiencyCategoryAndState = cms.vstring("passing"+X,"pass"),
             UnbinnedVariables = cms.vstring("mass"),
             BinnedVariables = cms.PSet(
-                pt  = cms.vdouble(3,15),
+                pt  = cms.vdouble(0,15),
                 eta = cms.vdouble(-2.5,2.5),
+                tag_Mu3      = cms.vstring("pass"),
                 hasValidHits = cms.vstring("pass"),
             ),
-            BinToPDFmap = cms.vstring("gaussPlusCubic")
-        ),
-        eff_eta = cms.PSet(
-            EfficiencyCategoryAndState = cms.vstring("passing","pass"),
+            BinToPDFmap = cms.vstring(sampleToPdfMap[X])
+        ))
+        setattr(process.TnP_Tracking.Efficiencies, "eff_eta"+X, cms.PSet(
+            EfficiencyCategoryAndState = cms.vstring("passing"+X,"pass"),
             UnbinnedVariables = cms.vstring("mass"),
             BinnedVariables = cms.PSet(
-                pt  = cms.vdouble(3,15),
-                eta = cms.vdouble(-2.5,-1.1,1.1,2.5),
+                pt  = cms.vdouble(0,15),
+                eta = cms.vdouble(-2.5, -1.1, 1.1, 2.5),
+                tag_Mu3      = cms.vstring("pass"),
                 hasValidHits = cms.vstring("pass"),
             ),
-            BinToPDFmap = cms.vstring("gaussPlusCubic")
-        )
-    )
+            BinToPDFmap = cms.vstring(sampleToPdfMap[X])
+        ))
+        setattr(process.TnP_Tracking.Efficiencies, "eff_pt_eta"+X, cms.PSet(
+            EfficiencyCategoryAndState = cms.vstring("passing"+X,"pass"),
+            UnbinnedVariables = cms.vstring("mass"),
+            BinnedVariables = cms.PSet(
+                pt  = cms.vdouble(0, 3.0, 5.0, 15.0),
+                abseta       = cms.vdouble(0, 1.1, 2.5),
+                tag_Mu3      = cms.vstring("pass"),
+                hasValidHits = cms.vstring("pass"),
+            ),
+            BinToPDFmap = cms.vstring(sampleToPdfMap[X])
+        ))
 
 process.p = cms.Path(
     process.TnP_Tracking
