646f823bed2d1d61869bcdc6493ce69e7c50b8a8
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-01-30 12:09:00 +0000

    Move tracking modules, add PF

--- python/common_modules_cff.py
+++ python/common_modules_cff.py
@@ -2,6 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from MuonAnalysis.TagAndProbe.nearbyMuonsInfo_cfi import nearbyMuonsInfo as tagProbeSeparation
 
+#########################################################################################
+##        Object counting modules                                                      ##
+#########################################################################################
+
 nverticesModule = cms.EDProducer("VertexMultiplicityCounter", 
     probes = cms.InputTag("tagMuons"),
     objects = cms.InputTag("offlinePrimaryVertices"),
@@ -24,16 +28,34 @@ njets30Module = cms.EDProducer("CandCleanedMultiplicityCounter",
     minProbeObjDR = cms.double(0.3),
 )
 
+#########################################################################################
+##        Tracking-related modules                                                     ##
+#########################################################################################
+
+## Now I have to define the passing probes for tracking
+## first remove low pt tracks which will not make muons anyway
+pCutTracks = cms.EDFilter("TrackSelector", 
+    src = cms.InputTag("generalTracks"),      
+    cut = cms.string("pt > 2 || (abs(eta) > 1 && p > 2)"),
+)
+tkTracks = cms.EDProducer("ConcreteChargedCandidateProducer", 
+    src = cms.InputTag("pCutTracks"),
+    particleType = cms.string("mu+"),
+)
+
+## Filter out the J/Psi's, to compute fake matching rate
 tkTracksNoJPsi = cms.EDProducer("CandidateResonanceInefficiencyCreator",
     src = cms.InputTag("tkTracks"),
     tags = cms.InputTag("tagMuons"),
     mass    = cms.double(3.096),
     massMin = cms.double(2.85), ## Should cut away
     massMax = cms.double(3.25), ## 99.5% of signal
-    onlyBestMatch = cms.bool(True),
+    onlyBestMatch = cms.bool(False),
     outputMode = cms.string("RefToBaseVector"),
 )
+tkTracksNoBestJPsi = tkTracksNoJPsi.clone(onlyBestMatch = True)
 
+## Filter out the J/Psi's, to compute fake matching rate
 tkTracksNoZ = cms.EDProducer("CandidateResonanceInefficiencyCreator",
     src = cms.InputTag("tkTracks"),
     tags = cms.InputTag("tagMuons"),
@@ -44,6 +66,45 @@ tkTracksNoZ = cms.EDProducer("CandidateResonanceInefficiencyCreator",
     outputMode = cms.string("RefToBaseVector"),
 )
 
-moreProbeInfo = cms.EDProducer("MoreProbeInfo",
+staToTkMatch = cms.EDProducer("MatcherUsingTracks",
+    src     = cms.InputTag("probeMuonsSta"),
+    matched = cms.InputTag("tkTracks"),  
+    algorithm = cms.string("byDirectComparison"), 
+    srcTrack     = cms.string("muon"),    srcState = cms.string("atVertex"), 
+    matchedTrack = cms.string("tracker"), matchedState = cms.string("atVertex"),
+    maxDeltaR        = cms.double(1.),   # large range in DR (we can tighten it later)
+    maxDeltaEta      = cms.double(0.4),  # small in eta, which is more precise
+    maxDeltaLocalPos = cms.double(100),
+    maxDeltaPtRel    = cms.double(5),   # |pt(sta) - pt(tk)|/pt(tk)
+    sortBy           = cms.string("deltaR"),
+    requireSameCharge = cms.bool(True),
+)
+staToTkMatchNoJPsi = staToTkMatch.clone(matched = 'tkTracksNoJPsi')
+staToTkMatchNoBestJPsi = staToTkMatch.clone(matched = 'tkTracksNoBestJPsi')
+staToTkMatchNoZ = staToTkMatch.clone(matched = 'tkTracksNoZ')
+
+staToTkMatchSequenceJPsi = cms.Sequence(
+    pCutTracks + 
+    tkTracks           * staToTkMatch           +
+    tkTracksNoJPsi     * staToTkMatchNoJPsi     +
+    tkTracksNoBestJPsi * staToTkMatchNoBestJPsi 
+)
+staToTkMatchSequenceZ = cms.Sequence(
+    pCutTracks +
+    tkTracks    * staToTkMatch    +
+    tkTracksNoZ * staToTkMatchNoZ     
+)
+
+#########################################################################################
+##        Other modules                                                                ##
+#########################################################################################
+
+muonDxyPVdzmin = cms.EDProducer("MuonDxyPVdzmin",
     probes = cms.InputTag("probeMuons"),
+)
+
+muonsPassingPF = cms.EDProducer("MuonsPassingPF",
+    muons = cms.InputTag("probeMuons"),
+    pf    = cms.InputTag("particleFlow"),
+    matchByReference = cms.bool(False), # set to true only if your probeMuons are a subset by reference of the "muons" collection
 )
