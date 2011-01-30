5e8d21738a884150ca55f44d9b4cb420c6a7a554
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-01-30 10:55:26 +0000

    Rename to something meaningful, and cleanup

--- plugins/MuonDxyPVdzmin.cc
+++ plugins/MuonDxyPVdzmin.cc
@@ -1,5 +1,6 @@
 // system include files
 #include <memory>
+#include <cmath>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -11,78 +12,36 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DataFormats/Common/interface/ValueMap.h"
-#include "DataFormats/PatCandidates/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonChamberMatch.h"
-#include "DataFormats/MuonDetId/interface/DTChamberId.h"
 #include <DataFormats/MuonReco/interface/Muon.h>
 
-#include <FWCore/Common/interface/TriggerNames.h>
-#include "DataFormats/Common/interface/TriggerResults.h"
-#include <DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h>
-#include "DataFormats/Math/interface/deltaPhi.h"
-
-#include <iostream>
-#include <map>
-
-#include "TH1D.h"
-
-
-// MARIO NTUPLIZER
-#include "DataFormats/BTauReco/interface/SoftLeptonTagInfo.h"
-#include "DataFormats/BTauReco/interface/SecondaryVertexTagInfo.h"
 #include "DataFormats/CLHEP/interface/AlgebraicObjects.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
-#include "DataFormats/Math/interface/Error.h"
-#include "DataFormats/TrackReco/interface/DeDxData.h"
 #include "DataFormats/TrackReco/interface/TrackBase.h"
 #include "DataFormats/BeamSpot/interface/BeamSpot.h"
-// #include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
-#include "DataFormats/PatCandidates/interface/TriggerEvent.h"
 
-#include "PhysicsTools/PatUtils/interface/TriggerHelper.h"
-
-#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
-#include "SimTracker/TrackAssociation/interface/TrackAssociatorBase.h"
-
-#include "TrackingTools/GeomPropagators/interface/AnalyticalImpactPointExtrapolator.h"
 #include "TrackingTools/IPTools/interface/IPTools.h"
-#include "TrackingTools/PatternTools/interface/TransverseImpactPointExtrapolator.h"
 #include "TrackingTools/Records/interface/TransientTrackRecord.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
-#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
-
-
 
 
 //
 // class declaration
 //
 
-class MoreProbeInfo : public edm::EDProducer {
+class MuonDxyPVdzmin : public edm::EDProducer {
 public:
-  explicit MoreProbeInfo(const edm::ParameterSet&);
-  ~MoreProbeInfo();
+  explicit MuonDxyPVdzmin(const edm::ParameterSet&);
+  ~MuonDxyPVdzmin();
 
 private:
   virtual void beginJob() ;
   virtual void produce(edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
-  float Phi( float&, float&);
 
   // ----------member data ---------------------------
   const edm::InputTag probes_;    
-  const edm::InputTag DT4Segments_;            
-
-  bool makeControlHisto_;
-  int MBx_;
-
-  const edm::InputTag trigRes_;
-
-  std::map<std::string,TH1D*> h1_;
-
 };
 
 //
@@ -97,25 +56,15 @@ private:
 //
 // constructors and destructor
 //
-MoreProbeInfo::MoreProbeInfo(const edm::ParameterSet& iConfig):
+MuonDxyPVdzmin::MuonDxyPVdzmin(const edm::ParameterSet& iConfig):
 probes_(iConfig.getParameter<edm::InputTag>("probes"))
 
 {
-
   produces<edm::ValueMap<float> >("dxyPVdzmin");
-  //register your products
-  /* Examples
-    produces<ExampleData2>();
-
-  //if do put with a label
-  produces<ExampleData2>("label");
-  */
-  //now do what ever other initialization is needed
-
 }
 
 
-MoreProbeInfo::~MoreProbeInfo()
+MuonDxyPVdzmin::~MuonDxyPVdzmin()
 {
 
   // do anything here that needs to be done at desctruction time
@@ -130,12 +79,12 @@ MoreProbeInfo::~MoreProbeInfo()
 
 // ------------ method called to produce the data  ------------
 void
-MoreProbeInfo::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+MuonDxyPVdzmin::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   using namespace edm;
 
   // read input
-  Handle<View<pat::Muon> > probes;
+  Handle<View<reco::Muon> > probes;
   iEvent.getByLabel(probes_,  probes);
   
   edm::Handle<std::vector<reco::Vertex> > primaryVerticesHandle;
@@ -151,7 +100,7 @@ MoreProbeInfo::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::vector<double> muon_dxy;
 
   // fill
-  View<pat::Muon>::const_iterator probe, endprobes = probes->end();
+  View<reco::Muon>::const_iterator probe, endprobes = probes->end();
 
   // loop on PROBES
   for (probe = probes->begin(); probe != endprobes; ++probe) {
@@ -177,8 +126,8 @@ MoreProbeInfo::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
         bsx = BSPosition.x();
         bsy = BSPosition.y();
         bsz = BSPosition.z();
-        Double_t dz_pv_bs = TMath::Abs(bsz-pvz);
-        Double_t rxy_pv_bs = TMath::Sqrt( (bsx-pvx)*(bsx-pvx) + (bsy-pvy)*(bsy-pvy) );
+        Double_t dz_pv_bs = std::abs(bsz-pvz);
+        Double_t rxy_pv_bs = std::sqrt( (bsx-pvx)*(bsx-pvx) + (bsy-pvy)*(bsy-pvy) );
         std::pair<bool,Measurement1D> dxy_fromIP = IPTools::absoluteTransverseImpactParameter(tTrack,primaryVerticesHandle->at(iVtx));
         std::pair<bool,Measurement1D> dxyz_fromIP = IPTools::absoluteImpactParameter3D(tTrack,primaryVerticesHandle->at(iVtx));
         Double_t dzmin_loop = sqrt(dxyz_fromIP.second.value()*dxyz_fromIP.second.value() - dxy_fromIP.second.value()*dxy_fromIP.second.value());
@@ -216,7 +165,7 @@ MoreProbeInfo::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-MoreProbeInfo::beginJob()
+MuonDxyPVdzmin::beginJob()
 {
 
 
@@ -224,8 +173,8 @@ MoreProbeInfo::beginJob()
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-MoreProbeInfo::endJob() {
+MuonDxyPVdzmin::endJob() {
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(MoreProbeInfo);
+DEFINE_FWK_MODULE(MuonDxyPVdzmin);
