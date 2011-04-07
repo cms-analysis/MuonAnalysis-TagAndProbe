938811b86a1aaf009ae8d41bc73e945b82d07bcb
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-04-07 20:16:28 +0000

    2011 cfg and macro

--- test/zmumu/fitMuonID.py
+++ test/zmumu/fitMuonID.py
@@ -1,11 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-
 ### USAGE:
-###    cmsRun fitMuonID_ICHEP.py <scenario>
+###    cmsRun fitMuonID.py <scenario> [ <id> [ <binning1> ... <binningN> ] ]
+###
 ### scenarios:
-###   - data_all:    will fit tnpJPsi_Data.root with bins suitable for the current data
-###   - datalike_mc: will fit tnpJPsi_{JPsiMuMu,ppMuX}_Spring10_0.117pb.root MC but
-###                  with same config as data
+###   - data_all (default)  
+###   - signal_mc
 
 import sys
 args = sys.argv[1:]
@@ -22,29 +21,36 @@ process.source = cms.Source("EmptySource")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
-process.TnP_MuonID = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
+Template = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
     NumCPU = cms.uint32(1),
-    SaveWorkspace = cms.bool(True),
-
-    InputFileNames = cms.vstring('tnpZ_Data.root'),
-    InputTreeName = cms.string("fitter_tree"),
-    InputDirectoryName = cms.string("tpTree"),
-    OutputFileName = cms.string("TnP_Z_MuonID_%s.root" % scenario),
+    SaveWorkspace = cms.bool(False),
 
     Variables = cms.PSet(
-        mass = cms.vstring("Tag-Probe Mass", "50", "130", "GeV/c^{2}"),
-        pt = cms.vstring("Probe p_{T}", "0", "1000", "GeV/c"),
-        abseta = cms.vstring("Probe |#eta|", "0", "2.5", ""),
-        tag_pt = cms.vstring("Tag p_{T}", "2.6", "1000", "GeV/c"),
+        mass = cms.vstring("Tag-muon Mass", "70", "130", "GeV/c^{2}"),
+        pt = cms.vstring("muon p_{T}", "0", "1000", "GeV/c"),
+        eta    = cms.vstring("muon #eta", "-2.5", "2.5", ""),
+        abseta = cms.vstring("muon |#eta|", "0", "2.5", ""),
+        phi    = cms.vstring("muon #phi at vertex", "-3.1416", "3.1416", ""),
+        charge = cms.vstring("muon charge", "-2.5", "2.5", ""),
+        tag_pt = cms.vstring("Tag p_{T}", "0", "1000", "GeV/c"),
+        tag_nVertices = cms.vstring("Number of vertices", "0", "999", ""),
+        isoTrk03Abs    = cms.vstring("Probe abs trk iso", "-2", "9999999", ""),
+        tag_combRelIso = cms.vstring("Tag comb rel iso", "-2", "9999999", ""),
     ),
 
     Categories = cms.PSet(
-        Calo = cms.vstring("",  "dummy[pass=1,fail=0]"),
-        TMA  = cms.vstring("",  "dummy[pass=1,fail=0]"),
-        Glb  = cms.vstring("",  "dummy[pass=1,fail=0]"),
-        VBTF = cms.vstring("", "dummy[pass=1,fail=0]"),
-        Isol = cms.vstring("",  "dummy[pass=1,fail=0]"),
-        Mu9  = cms.vstring("",  "dummy[pass=1,fail=0]"),
+        Glb   = cms.vstring("Global", "dummy[pass=1,fail=0]"),
+        VBTF     = cms.vstring("VBTFLike", "dummy[pass=1,fail=0]"),
+        VBTFold  = cms.vstring("VBTFLike", "dummy[pass=1,fail=0]"),
+        TMOSL = cms.vstring("TMOneStationLoose", "dummy[pass=1,fail=0]"),
+        TMOST = cms.vstring("TMOneStationTight", "dummy[pass=1,fail=0]"),
+        PF    = cms.vstring("PF Muon", "dummy[pass=1,fail=0]"),
+        TM    = cms.vstring("Tracker Muon", "dummy[pass=1,fail=0]"),
+        TMA   = cms.vstring("Arbitrated Tracker Muon", "dummy[pass=1,fail=0]"),
+        Isol  = cms.vstring("Comb rel iso", "dummy[pass=1,fail=0]"),
+        IsolTk3 = cms.vstring("Tk Abs Iso < 3", "dummy[pass=1,fail=0]"),
+        Mu15  = cms.vstring("MC true", "dummy[true=1,false=0]"),
+        mcTrue = cms.vstring("MC true", "dummy[true=1,false=0]"),
     ),
 
     PDFs = cms.PSet(
@@ -54,41 +60,142 @@ process.TnP_MuonID = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
             "Exponential::backgroundFail(mass, lf[0,-5,5])",
             "efficiency[0.9,0,1]",
             "signalFractionInPassing[0.9]"
+        ),
+        vpvPlusExpo = cms.vstring(
+            "Voigtian::signal1(mass, mean1[90,80,100], width[2.495], sigma1[2,1,3])",
+            "Voigtian::signal2(mass, mean2[90,80,100], width,        sigma2[4,2,10])",
+            "SUM::signal(vFrac[0.8,0,1]*signal1, signal2)",
+            "Exponential::backgroundPass(mass, lp[-0.1,-1,0.1])",
+            "Exponential::backgroundFail(mass, lf[-0.1,-1,0.1])",
+            "efficiency[0.9,0,1]",
+            "signalFractionInPassing[0.9]"
         )
     ),
+
+    binnedFit = cms.bool(True),
+    binsForFit = cms.uint32(40),
 
     Efficiencies = cms.PSet(), # will be filled later
 )
 
-ONE_BIN = cms.PSet(
-    pt     = cms.vdouble(20, 100),
-    abseta = cms.vdouble( 0, 2.4)
+PT_ETA_BINS = cms.PSet(
+    pt     = cms.vdouble(  10, 20, 30, 40, 60, 100 ),
+    abseta = cms.vdouble(  0.0, 1.2, 2.4)
 )
-PT_BINS  = ONE_BIN.clone(pt = cms.vdouble(15, 35, 100))
-ETA_BINS = ONE_BIN.clone(abseta = cms.vdouble(0, 1.2, 2.4))
+ETA_BINS = cms.PSet(
+    pt  = cms.vdouble(20,100),
+    eta = cms.vdouble(-2.4, -2.1, -1.6, -1.1, -0.6, 0, 0.6, 1.1, 1.6, 2.1, 2.4),
+)
+ETA_PHI_BINS = ETA_BINS.clone(
+   eta = cms.vdouble(-2.4, -1.6, -8.0, 0, 0.8, 1.6, 2.4),
+   phi = cms.vdouble(*[3.1416*i/3.0 for i in range(-3,4)]), 
+)
+VTX_BINS  = cms.PSet(
+    pt     = cms.vdouble(  20, 120 ),
+    abseta = cms.vdouble(  0.0, 2.4),
+    tag_nVertices = cms.vdouble(0.5,2.5,4.5,6.5,8.5,10.5,12.5,14.5,16.5)
+)
 
-if scenario == "data_all":
-    process.TnP_MuonID.binsForMassPlots = cms.uint32(20)
+ETA_BINS_FINE = cms.PSet(
+    pt  = cms.vdouble(20,100),
+    eta = cms.vdouble(*[x/10. for x in xrange(-24,25,1)]),
+)
+OVERALL = cms.PSet(
+    pt  = cms.vdouble(20,100),
+    abseta = cms.vdouble(0.0, 2.4),
+)
+OVERALL_ABSETA = cms.PSet(
+    pt  = cms.vdouble(20,100),
+    abseta = cms.vdouble(0.0, 1.2, 2.4),
+)
 
-if scenario == "datalike_mc":
-    process.TnP_MuonID.InputFileNames = [ "tnpZ_MC.root", ]
+OVERALL_ENDCAPS21 = cms.PSet(
+    pt  = cms.vdouble(20,100),
+    abseta = cms.vdouble(1.2, 2.1),
+)
 
-ALLBINS=[("all",ONE_BIN),("pt",PT_BINS),("eta",ETA_BINS)]
-for T in [ "Glb", "VBTF", "Isol" ]:
-    for  X,B in ALLBINS:
-        setattr(process.TnP_MuonID.Efficiencies, T+"_"+X, cms.PSet(
-            EfficiencyCategoryAndState = cms.vstring(T,"pass"),
+
+CHARGE = cms.PSet(
+    pt     = cms.vdouble(20,100),
+    abseta = cms.vdouble(0.0, 2.4),
+    charge = cms.vdouble(-2,0,2),
+)
+
+
+
+#PREFIX="/data/gpetrucc/7TeV/tnp/2011.02.17/"
+PREFIX="/data/gpetrucc/7TeV/tnp/2011-04/"
+process.TnP_MuonID = Template.clone(
+    InputFileNames = cms.vstring(PREFIX+'tnpZ_2011A_MuonPhys.root'),
+    InputTreeName = cms.string("fitter_tree"),
+    InputDirectoryName = cms.string("tpTree"),
+    OutputFileName = cms.string("TnP_MuonID_%s.root" % scenario),
+    Efficiencies = cms.PSet(),
+)
+
+if "JSON" in scenario and "data" in scenario:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_2011A.root" ]
+if "38X" in scenario and "data" in scenario:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_Data_Nov4B.root" ]
+if "39X" in scenario and "data" in scenario:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_Data_Dec22B.root" ]
+if scenario.find("signal_mc") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_DYToMMpowhegZ2_Spring11PU.root" ]
+if scenario.find("some_mc") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_MCDYPoweg_38X_dzIso.334pb.root" ]
+if scenario.find("39X_mc") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_MC_39XDY_1.root" ]
+if scenario.find("some_mc2") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_DYToMMpowhegZ2_Fall10PU.root" ]
+if scenario.find("39X_mc2") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_DYToMMpowhegZ2_Winter10PU.root" ]
+if scenario.find("realistic_mc") != -1:
+    process.TnP_MuonID.InputFileNames = [ PREFIX+"tnpZ_MCDYPoweg_38X_dzIso.334pb.root", 
+                                          PREFIX+"tnpZ_MC_WJetsPU.334pb.root", 
+                                          PREFIX+"tnpZ_MC_QCDMuPt15PU.334pb.root", ]
+if "tag35" in scenario:
+    process.TnP_MuonID.Variables.tag_pt[1]='35'
+
+IDS = [ "TMOST", "VBTF", "PF" ]
+IDS += [ "Glb" ]
+ALLBINS = [("pt_abseta",PT_ETA_BINS),("eta", ETA_BINS)]
+ALLBINS += [ ("vtx",VTX_BINS)]
+#ALLBINS+=[("eta_fine",ETA_BINS_FINE)]
+ALLBINS += [("overall",OVERALL), ("charge",CHARGE), ("overall_abseta",OVERALL_ABSETA),("overall_endcaps21",OVERALL_ENDCAPS21)]
+ALLBINS+=[("eta_phi",ETA_PHI_BINS)]
+
+if len(args) > 1 and args[1] not in IDS: IDS += [ args[1] ]
+for ID in IDS:
+    if len(args) > 1 and ID != args[1]: continue
+    for X,B in ALLBINS:
+        if len(args) > 2 and X not in args[2:]: continue
+        module = process.TnP_MuonID.clone(OutputFileName = cms.string("TnP_MuonID_%s_%s_%s.root" % (scenario, ID, X)))
+        shape = "vpvPlusExpo"
+        if X.find("eta") != -1 and X.find("abseta") == -1: shape = "voigtPlusExpo"
+        if X.find("pt_abseta") != -1: module.Variables.mass[1]="77";
+        if X.find("overall") != -1: module.binsForFit = 120
+        DEN = B.clone(); num = ID;
+        if "_from_" in ID:
+            parts = ID.split("_from_")
+            num = parts[0]
+            setattr(DEN, parts[1], cms.vstring("pass"))
+        if scenario.find("tagiso") != -1:  
+            DEN.tag_combRelIso = cms.vdouble(-1, 0.1)
+        if scenario.find("probeiso") != -1:
+            DEN.isoTrk03Abs = cms.vdouble(-1, 3)
+        #if scenario.find("calo") != -1: DEN.caloCompatibility = cms.vdouble(0.9,1.1)  # same as above, I think.
+        setattr(module.Efficiencies, ID+"_"+X, cms.PSet(
+            EfficiencyCategoryAndState = cms.vstring(num,"pass"),
             UnbinnedVariables = cms.vstring("mass"),
-            BinnedVariables = B,
-            BinToPDFmap = cms.vstring("voigtPlusExpo")
+            BinnedVariables = DEN,
+            BinToPDFmap = cms.vstring(shape)
         ))
-for  X,B in ALLBINS:
-    setattr(process.TnP_MuonID.Efficiencies, "VBTF_Isol_"+X, cms.PSet(
-        EfficiencyCategoryAndState = cms.vstring("VBTF","pass","Isol","pass"),
-        UnbinnedVariables = cms.vstring("mass"),
-        BinnedVariables = B,
-        BinToPDFmap = cms.vstring("voigtPlusExpo")
-    ))
-
-process.p = cms.Path(process.TnP_MuonID)
+        if scenario.find("mc") != -1:
+            setattr(module.Efficiencies, ID+"_"+X+"_mcTrue", cms.PSet(
+                EfficiencyCategoryAndState = cms.vstring(num,"pass"),
+                UnbinnedVariables = cms.vstring("mass"),
+                BinnedVariables = DEN.clone(mcTrue = cms.vstring("true"))
+            ))
+        setattr(process, "TnP_MuonID_"+ID+"_"+X, module)        
+        setattr(process, "run_"+ID+"_"+X, cms.Path(module))
 
---
+++ test/zmumu/plotMuonID.cxx
@@ -0,0 +1,258 @@
+#include <TCanvas.h>
+#include <TPad.h>
+#include "../jpsi/plotUtil.cxx"
+
+void plotMuonIDData() ;
+
+void plotMuonID(TString scenario="data") {
+
+    prefix = prefix+scenario+"/";
+    gSystem->mkdir(prefix,true);
+
+    gROOT->ProcessLine(".x /afs/cern.ch/user/g/gpetrucc/cpp/tdrstyle.cc");
+    gStyle->SetOptStat(0);
+    c1 = new TCanvas("c1","c1");
+
+    if (gROOT->GetListOfFiles()->GetEntries() == 2) {
+        ref = (TFile *) gROOT->GetListOfFiles()->At(1);
+        ((TFile*) gROOT->GetListOfFiles()->At(0))->cd();
+    }
+
+    //fOut = TFile::Open(prefix+"plots.root", "RECREATE");
+    ((TFile*) gROOT->GetListOfFiles()->At(0))->cd();
+
+    doRatioPlot = false;
+    doDiffPlot = false;
+    doPdf = true;
+    doSquare = true; yMin = 0.7; yMax = 1.048;
+    datalbl = "Data, 2011";
+    reflbl  = "Sim., Spring11";
+    preliminary = "CMS Preliminary,   #sqrt{s} = 7 TeV";
+
+    if (scenario.Contains("any_vs_iso")) {
+        datalbl = "All tracks";
+        reflbl  = "Iso. tracks";
+        yMinD = -0.2; yMaxD = 0.2;
+        yMinR =  0.8; yMaxR = 1.2;
+    } else if (scenario.Contains("signal_mc_vs_realistic_mc")) {
+        datalbl = "Sim. signal only";
+        reflbl  = "Sim. w/ backgrounds";
+        yMinD = -0.2; yMaxD = 0.2;
+        yMinR =  0.8; yMaxR = 1.2;
+    } else if (scenario.Contains("38X_vs_39X")) {
+        datalbl = "3.9.X";
+        reflbl  = "3.8.X";
+        yMinD = -0.2; yMaxD = 0.2;
+        yMinR =  0.8; yMaxR = 1.2;
+    } else if (scenario.Contains("_vs_2010")) {
+        datalbl = "Data, 2011A";
+        reflbl  = "Data, 2010B";
+        yMinD = -0.2; yMaxD = 0.2;
+        yMinR =  0.8; yMaxR = 1.2;
+    }
+
+    plotMuonIDData();
+}
+
+void plotMuonIDData() {
+    retitle = "Efficiency";
+
+    const int nids  = 8;
+    const char *ids[nids]    = { "Glb",    "TMOST",   "VBTF",  "PF", "TM",      "TMA",        "TMOSL",  "Isol_from_VBTF" };
+    const char *titles[nids] = { "Global",  "Soft",   "Tight", "PF", "Tracker", "TrackerArb", "Softer", "Isolation" };
+
+   
+    for (int i = 0; i < nids; ++i) {
+        TString idname(ids[i]);
+        retitle = TString(titles[i])+" muon efficiency";
+        TDirectory *fit_pt_eta = gFile->GetDirectory(basedir+"/"+idname+"_pt_abseta/");
+        if (fit_pt_eta == 0) { /*if (i == 0) { gFile->GetDirectory(basedir)->ls(); } ;*/ continue; }
+        yMin = 0.0; yMax = 1.1;
+        if (ref != 0) {
+            TDirectory *ref_pt_eta = ref->GetDirectory(basedir+"/"+idname+"_pt_abseta/");
+            extraSpam = "        |#eta| < 1.2"; refstack(fit_pt_eta, ref_pt_eta, idname+"_pt_barrel",  "pt_PLOT_abseta_bin0");
+            extraSpam = "  1.2 < |#eta| < 2.4"; refstack(fit_pt_eta, ref_pt_eta, idname+"_pt_endcaps", "pt_PLOT_abseta_bin1");
+            TDirectory *mc_pt_eta  = ref->GetDirectory(basedir+"/"+idname+"_pt_abseta"+"_mcTrue/");
+            if (mc_pt_eta) {
+                extraSpam = "             |#eta| < 1.2"; refstack3(fit_pt_eta, ref_pt_eta, mc_pt_eta, idname+"_pt_barrel_3",  "pt_PLOT_abseta_bin0");
+                extraSpam = "       1.2 < |#eta| < 2.4"; refstack3(fit_pt_eta, ref_pt_eta, mc_pt_eta, idname+"_pt_endcaps_3", "pt_PLOT_abseta_bin1");
+            }
+        } else {
+            TDirectory *mc_pt_eta  = gFile->GetDirectory(basedir+"/"+idname+"_pt_abseta_mcTrue/");
+            if (mc_pt_eta) {
+                TString databk = datalbl, refbk = reflbl;
+                datalbl = "T&P fit"; reflbl = "Sim. truth";
+                extraSpam = "        |#eta| < 1.2"; mcstack(fit_pt_eta, mc_pt_eta, idname+"_pt_barrel",  "pt_PLOT_abseta_bin0");
+                extraSpam = "  1.2 < |#eta| < 2.4"; mcstack(fit_pt_eta, mc_pt_eta, idname+"_pt_endcaps", "pt_PLOT_abseta_bin1");
+                reflbl = refbk; datalbl = databk;
+            } else {
+                extraSpam = "        |#eta| < 1.2"; single(fit_pt_eta, idname+"_pt_barrel",  "pt_PLOT_abseta_bin0");
+                extraSpam = "  1.2 < |#eta| < 2.4"; single(fit_pt_eta, idname+"_pt_endcaps", "pt_PLOT_abseta_bin1");
+            }
+        }
+        //doCanvas(fit_pt_eta, 1, 7, idname+"_barrel_pt_%d",   "abseta_bin0__pt_bin%d_");
+        //doCanvas(fit_pt_eta, 1, 7, idname+"_endcaps_pt_%d",  "abseta_bin1__pt_bin%d_");
+
+        yMin = 0.7; yMax = 1.049;
+        TDirectory *fit_eta = gFile->GetDirectory(basedir+"/"+idname+"_eta/");
+        if (fit_eta) {
+            if (ref != 0) {
+                extraSpam = "    p_{T} > 20 GeV"; retitleX = "muon #eta";
+                TDirectory *ref_eta = ref->GetDirectory(basedir+"/"+idname+"_eta/");
+                refstack(fit_eta, ref_eta, idname+"_eta",  "eta_PLOT_");
+                TDirectory *mc_eta  = ref->GetDirectory(basedir+"/"+idname+"_eta_mcTrue/");
+                if (mc_eta) {
+                    refstack3(fit_eta, ref_eta, mc_eta, idname+"_eta_3",  "eta_PLOT_");
+                }
+            } else {
+                TDirectory *mc_eta  = gFile->GetDirectory(basedir+"/"+idname+"_eta_mcTrue/");
+                if (mc_eta) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_eta, mc_eta, idname+"_eta",  "eta_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_eta, idname+"_eta",  "eta_PLOT_");
+                }
+            }
+            //doCanvas(fit_eta, 1, 27, idname+"_eta_%d",   "eta_bin%d_");
+        }
+
+        yMin = 0.0; yMax = 1.1;
+        fit_eta = gFile->GetDirectory(basedir+"/"+idname+"_eta_fine/");
+        if (fit_eta) {
+            if (ref != 0) {
+                extraSpam = "    p_{T} > 20 GeV"; retitleX = "muon #eta";
+                TDirectory *ref_eta = ref->GetDirectory(basedir+"/"+idname+"_eta_fine/");
+                refstack(fit_eta, ref_eta, idname+"_eta_fine",  "eta_PLOT_");
+                TDirectory *mc_eta  = ref->GetDirectory(basedir+"/"+idname+"_eta_fine_mcTrue/");
+                if (mc_eta) {
+                    refstack3(fit_eta, ref_eta, mc_eta, idname+"_eta_fine_3",  "eta_PLOT_");
+                }
+            } else {
+                TDirectory *mc_eta  = gFile->GetDirectory(basedir+"/"+idname+"_eta_fine_mcTrue/");
+                if (mc_eta) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_eta, mc_eta, idname+"_eta_fine",  "eta_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_eta, idname+"_eta_fine",  "eta_PLOT_");
+                }
+            }
+            if (ref == 0) {
+                //doCanvas(fit_eta, 1, 27, idname+"_eta_fine_%d",   "eta_bin%d_");
+            }
+        }
+
+
+        yMin = 0.85; yMax = 1.019;
+        TDirectory *fit_vtx = gFile->GetDirectory(basedir+"/"+idname+"_vtx/");
+        if (fit_vtx) {
+            TDirectory *ref_vtx = ref ? ref->GetDirectory(basedir+"/"+idname+"_vtx/") : 0;
+            if (ref_vtx) {
+                refstack(fit_vtx, ref_vtx, idname+"_vtx",  "tag_nVertices_PLOT_");
+                TDirectory *mc_vtx  = ref->GetDirectory(basedir+"/"+idname+"_vtx_mcTrue/");
+                if (mc_vtx) {
+                    refstack3(fit_vtx, ref_vtx, mc_vtx, idname+"_vtx_3",  "tag_nVertices_PLOT_");
+                }
+            } else {
+                TDirectory *mc_vtx  = gFile->GetDirectory(basedir+"/"+idname+"_vtx_mcTrue/");
+                if (mc_vtx) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_vtx, mc_vtx, idname+"_vtx",  "tag_nVertices_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_vtx, idname+"_vtx",  "tag_nVertices_PLOT_");
+                }
+            }
+            if (ref == 0) doCanvas(fit_vtx, 1, 27, idname+"_vtx_%d",   "tag_nVertices_bin%d_");
+        }
+
+        yMin = 0.85; yMax = 1.019;
+        TDirectory *fit_charge = gFile->GetDirectory(basedir+"/"+idname+"_charge/");
+        if (fit_charge) {
+            TDirectory *ref_charge = ref ? ref->GetDirectory(basedir+"/"+idname+"_charge/") : 0;
+            if (ref_charge != 0) {
+                refstack(fit_charge, ref_charge, idname+"_charge",  "charge_PLOT_");
+                TDirectory *mc_charge  = ref->GetDirectory(basedir+"/"+idname+"_charge_mcTrue/");
+                if (mc_charge) {
+                    refstack3(fit_charge, ref_charge, mc_charge, idname+"_charge_3",  "charge_PLOT_");
+                }
+            } else {
+                TDirectory *mc_charge  = gFile->GetDirectory(basedir+"/"+idname+"_charge_mcTrue/");
+                if (mc_charge) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_charge, mc_charge, idname+"_charge",  "charge_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_charge, idname+"_charge",  "charge_PLOT_");
+                }
+            }
+            if (ref == 0) {
+                //doCanvas(fit_charge, 1, 2, idname+"_charge_%d",   "charge_bin%d_");
+            }
+        }
+
+        yMin = 0.941; yMax = 1.009;
+        if (idname.Contains("VBTF")) { yMin -= 0.02; yMax  -= 0.02; }
+
+        TDirectory *fit_overall = gFile->GetDirectory(basedir+"/"+idname+"_overall/");
+        if (fit_overall) {
+            TDirectory *ref_overall = ref ? ref->GetDirectory(basedir+"/"+idname+"_overall/") : 0;
+            if (ref_overall != 0) {
+                refstack(fit_overall, ref_overall, idname+"_overall",  "abseta_PLOT_");
+                TDirectory *mc_overall  = ref->GetDirectory(basedir+"/"+idname+"_overall_mcTrue/");
+                if (mc_overall) {
+                    refstack3(fit_overall, ref_overall, mc_overall, idname+"_overall_3",  "abseta_PLOT_");
+                }
+            } else {
+                TDirectory *mc_overall  = gFile->GetDirectory(basedir+"/"+idname+"_overall_mcTrue/");
+                if (mc_overall) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_overall, mc_overall, idname+"_overall",  "abseta_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_overall, idname+"_overall",  "abseta_PLOT_");
+                }
+            }
+            //doCanvas(fit_overall, 1, 1, idname+"_overall_%d",   "abseta_bin%d_");
+        }
+
+        yMin = 0.85; yMax = 1.019;
+        TDirectory *fit_overall_abseta = gFile->GetDirectory(basedir+"/"+idname+"_overall_abseta/");
+        if (fit_overall_abseta) {
+            TDirectory *ref_overall_abseta = ref ? ref->GetDirectory(basedir+"/"+idname+"_overall_abseta/") : 0;
+            if (ref_overall_abseta) {
+                extraSpam = "    p_{T} > 20 GeV"; retitleX = "muon |#eta|";
+                refstack(fit_overall_abseta, ref_overall_abseta, idname+"_overall_abseta",  "abseta_PLOT_");
+            } else {
+                TDirectory *mc_overall_abseta  = gFile->GetDirectory(basedir+"/"+idname+"_overall_abseta_mcTrue/");
+                if (mc_overall_abseta) {
+                    TString databk = datalbl, refbk = reflbl;
+                    datalbl = "T&P fit"; reflbl = "Sim. truth";
+                    mcstack(fit_overall_abseta, mc_overall_abseta, idname+"_overall_abseta",  "abseta_PLOT_");
+                    reflbl = refbk; datalbl = databk;
+                } else {
+                    single(fit_overall_abseta, idname+"_overall_abseta",  "abseta_PLOT_");
+                }
+            }
+            doCanvas(fit_overall_abseta, 1, 27, idname+"_overall_abseta_%d",   "abseta_bin%d_");
+        }
+        TDirectory *fit_overall_endcaps21 = gFile->GetDirectory(basedir+"/"+idname+"_overall_endcaps21/");
+        if (fit_overall_endcaps21) {
+            TDirectory *ref_overall_endcaps21 = ref ? ref->GetDirectory(basedir+"/"+idname+"_overall_endcaps21/") : 0;
+            if (ref_overall_endcaps21) {
+                extraSpam = "    p_{T} > 20 GeV"; retitleX = "muon |#eta|";
+                refstack(fit_overall_endcaps21, ref_overall_endcaps21, idname+"_overall_endcaps21",  "abseta_PLOT_");
+            } else {
+                single(fit_overall_endcaps21, idname+"_overall_endcaps21",  "abseta_PLOT_");
+            }
+            doCanvas(fit_overall_endcaps21, 1, 27, idname+"_overall_endcaps21_%d",   "abseta_bin%d_");
+        }
+    }
+}
+
