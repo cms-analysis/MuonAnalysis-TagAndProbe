b9c69e8d6d81c7aadfccbdca412399998bd8326b
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-04-07 20:14:28 +0000

    Allow filtering what to merge

--- test/jpsi/merge.sh
+++ test/jpsi/merge.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-F=$1;
+F=$1; shift;
 if [[ "$F" == "" ]]; then echo "Usage: merge.sh file[.root]  (will merge file_*.root into file.root)"; exit 1; fi;
 F="${F/.root/}";
-echo "Will merge $(ls ${F}_[A-Z]*.root) into $F.root"
-hadd -f $F.root ${F}_[A-Z]*.root > /dev/null 
+FILES=$(ls ${F}_[A-Z]*.root);
+if [[ "X$1" != "X" ]]; then
+    FILES=$(ls ${F}_[A-Z]*.root | grep $*);
+fi;
+echo "Will merge $FILES into $F.root"
+hadd -f $F.root $FILES > /dev/null 
