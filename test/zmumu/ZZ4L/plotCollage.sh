4d17e928972a7f5571717cbe18e4c0a3c2e98183
cmsbuild <>
2012-08-05 20:37:18 +0000

    This commit was manufactured by cvs2git to create tag 'MuonAnalysis- TagAndProbe-V08-02-02'.
    Sprout from master 2012-08-05 20:37:17 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Add sorting L1 matches by quality'
    Delete:
        test/zmumu/ZZ4L/collageMuonID_ZZ4L.cxx
        test/zmumu/ZZ4L/fitAll.sh
        test/zmumu/ZZ4L/fitJPsi.sh
        test/zmumu/ZZ4L/fitMuonID.py
        test/zmumu/ZZ4L/fitMuonID_JPsi.py
        test/zmumu/ZZ4L/overallMuonID_ZZ4L.cxx
        test/zmumu/ZZ4L/plotAll.sh
        test/zmumu/ZZ4L/plotCollage.sh
        test/zmumu/ZZ4L/plotMuonID_JPsi_ZZ4L.cxx
        test/zmumu/ZZ4L/plotMuonID_ZZ4L.cxx
        test/zmumu/ZZ4L/plotUtil.cxx
        test/zmumu/ZZ4L/preprocessing/addEAIso.cxx
        test/zmumu/ZZ4L/preprocessing/addMVAIso.cxx
        test/zmumu/ZZ4L/preprocessing/addNVtxWeight.cxx
        test/zmumu/ZZ4L/preprocessing/addTriggerEmu.cxx
        test/zmumu/ZZ4L/preprocessing/loadMVAlibs.cxx
        test/zmumu/ZZ4L/preprocessing/slimTree.cxx
        test/zmumu/ZZ4L/preprocessing/subTree.C
        test/zmumu/ZZ4L/preprocessing/suite.sh
        test/zmumu/ZZ4L/tdrstyle.cc

--- test/zmumu/ZZ4L/plotCollage.sh
+++
@@ -1,1 +0,0 @@
-for Y in 2012; do root.exe -b -q -l  plots/muonid_5fb/jpsi/${Y}_fitJPsi.root plots/muonid_5fb/zmumu/${Y}_fitZ.root collageMuonID_ZZ4L.cxx; done
--- test/zmumu/ZZ4L/preprocessing/loadMVAlibs.cxx
+++
@@ -1,3 +0,0 @@
-void loadMVAlibs() {
-    gSystem->Load("libMuonMuonAnalysisTools.so");
-}
