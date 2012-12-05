69ad874596bde1567c3abfae7a8f273469b7b067
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2012-12-05 20:41:00 +0000

    create cfgs to send all jobs

---
+++ test/zmumu/production/mkjobs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from sys import argv
+from re import *
+import os, os.path
+
+print argv
+if len(argv) != 4: raise RuntimeError, "Usage: mkjobs.py py crabcfg datasets"
+pyname   = argv[1]
+py       = "".join([l for l in open(argv[1],'r')])
+cfgname  = argv[2]
+cfg      = "".join([l for l in open(argv[2],'r')])
+datasets = open(argv[3])
+
+jsondir = os.environ['CMSSW_BASE']+"/src/MuonAnalysis/TagAndProbe/test/zmumu/production/";
+
+def cfgsub(cfg, dict):
+    for key, value in dict.iteritems():
+        cfg = sub(compile("^\\s*%s\\s*=\\s*\\S+" % key, MULTILINE), "%s = %s" % (key,value), cfg)
+    return cfg
+
+for row in datasets:
+    if row[0] == "#": continue
+    (nick, dataset, tag, json, jobs) = row.split()
+    pyout = pyname.replace(".py", ".%s.py" % nick)
+    print "Will prepare python %s to run with global tag %s" % (pyout, tag)
+    mypy = cfgsub(py, {"process.GlobalTag.globaltag":("'%s::All'"%tag)} )
+    pyhandle = open(pyout, "w")
+    pyhandle.write(mypy)
+    pyhandle.close()    
+    cfgout = os.path.basename(cfgname.replace(".cfg", ".%s.cfg" % nick))
+    print "Will prepare crab cfg %s to run %s on dataset %s (%s jobs, json %s)" % (cfgout, pyout, dataset, jobs, json)
+    mycfg = cfgsub(cfg, {'number_of_jobs':jobs, 'datasetpath':dataset, 'lumi_mask':jsondir+json, 'pset':pyout} )
+    if json in ["-","none","MC"]:
+        mycfg = sub(r"^lumi_mask\s*=\s*\S+", "", mycfg.replace("total_number_of_lumis","total_number_of_events"))
+    cfghandle = open(cfgout, "w")
+    cfghandle.write(mycfg)
+    cfghandle.close()    
