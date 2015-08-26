b10a7634a85f3372caf0a7ba5654664fda5934cb
Carlo Battilana <carlo.battilana@gmail.com>
2015-08-26 10:48:01 +0200

    Minor fix: processing all events

--- test/zmumu/tp_from_aod_Data.py
+++ test/zmumu/tp_from_aod_Data.py
@@ -13,7 +13,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring(),
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )    
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )    
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
